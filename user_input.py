52ea6289cb01f33934626cb40fe3e192db5ca28a
ochi3ng <johnochi3ng@gmail.com>
2024-11-05 10:27:18 +0300

    Introduction to python Add user input program

---
+++ user_input.py
@@ -0,0 +1,7 @@
+# Asking for user's details
+name = input("What's your name? ")
+age = input("How old are you? ")
+location = input("Where do you live? ")
+
+# Printing a personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}. Nice to meet you. ")
